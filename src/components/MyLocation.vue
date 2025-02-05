df22a2b5c409fe2545da8194256d0dd7b76c3c3e
maelgllt <mael.guilloteau@outlook.fr>
2025-02-05 14:43:50 +0100

    fix manifest + carte localisation

--- src/components/MyLocation.vue
+++ src/components/MyLocation.vue
@@ -1,7 +1,8 @@
 <template>
   <div v-if="isOnline">
-    <p>Latitude: {{ latitude }}</p>
-    <p>Longitude: {{ longitude }}</p>
+    <p v-if="latitude">Latitude: {{ latitude }}</p>
+    <p v-if="longitude">Longitude: {{ longitude }}</p>
+    <div id="map"></div>
   </div>
   <div v-else>
     <p>Vous êtes hors ligne. Veuillez vous connecter à Internet pour voir votre localisation.</p>
@@ -9,12 +10,17 @@
 </template>
 
 <script>
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+
 export default {
   data() {
     return {
       latitude: null,
       longitude: null,
       isOnline: navigator.onLine,
+      map: null,
+      marker: null,
     };
   },
   mounted() {
@@ -33,6 +39,7 @@ export default {
           (position) => {
             this.latitude = position.coords.latitude;
             this.longitude = position.coords.longitude;
+            this.initMap();
           },
           (error) => {
             alert('Error getting location:', error);
@@ -45,9 +52,34 @@ export default {
     updateOnlineStatus() {
       this.isOnline = navigator.onLine;
     },
+    initMap() {
+      if (this.map) {
+        this.map.setView([this.latitude, this.longitude], 13);
+        if (this.marker) {
+          this.marker.setLatLng([this.latitude, this.longitude]);
+        } else {
+          this.marker = L.marker([this.latitude, this.longitude]).addTo(this.map)
+            .bindPopup('Votre position actuelle').openPopup();
+        }
+        return;
+      }
+
+      this.map = L.map('map').setView([this.latitude, this.longitude], 13);
+
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; OpenStreetMap contributors',
+      }).addTo(this.map);
+
+      this.marker = L.marker([this.latitude, this.longitude]).addTo(this.map)
+        .bindPopup('Votre position actuelle').openPopup();
+    },
   },
 };
 </script>
 
 <style scoped>
+#map {
+  width: 100%;
+  height: 400px;
+}
 </style>
